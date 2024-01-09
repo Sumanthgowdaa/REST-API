78b16b6fcd7616f7cc824508c171bcf06c6c5eb3
Sumanthgowdaa <sumanthsilence@gmail.com>
2024-01-09 17:10:24 +0530

    committed

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const { StatusCodes } = require('http-status-codes')
-
-const connectDb = require('./db/connect')
-
 const PORT = process.env.PORT
+const connectDb = require('./db/connect')
+const expressFileUpload = require('express-fileupload')
+
+
 
 //instance
 const app =express()
@@ -18,25 +19,27 @@ app.use(express.json())   //json format of data
 //middleware
 app.use(cors())  //cross origin resource origin
 app.use(cookieParser(process.env.ACCESS_SECRET)) 
+app.use(expressFileUpload({
+    limits: {fileSize: 10 * 1024 * 1024},
+    useTempFiles: true
+}));
 
 
 // api route
 app.use(`/api/auth`, require('./router/authRoute'))
+app.use(`/api/file`,require('./router/fileRoute'))
 
 //default route
 app.use(`*`,(req,res) =>{
-<<<<<<< HEAD
+
     res.status(StatusCodes.SERVICE_UNAVAILABLE).json({msg:``})
-=======
+
     res.status(StatusCodes.SERVICE_UNAVAILABLE).json({msg:`Requested service path not available`})
->>>>>>> 24ab9d0513d4b1fd2f568c10e2b7dc26e07ef8ea
+
 })
 //server Listen
 app.listen(PORT,() => {
     connectDb()
     console.log(`server has started and running at http://localhost:${PORT}`)
-<<<<<<< HEAD
+
 })
-=======
-})
->>>>>>> 24ab9d0513d4b1fd2f568c10e2b7dc26e07ef8ea
---
+++ model/fileModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const fileSchema = new mongoose.Schema({
+    user:{
+        type: Object,
+        required: true
+    },
+    info: {
+        type: Object,
+        required:true
+    },
+    isActive: {
+        type:Boolean,
+        default:true
+    } 
+},{
+    collection:'files',
+    timestamps: true
+})
+
+module.exports = mongoose.model('FileSchema',fileSchema)
