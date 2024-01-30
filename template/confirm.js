4d4e22dfe911fc06d2a04a7525ce5b07b8dd1de1
Sumanthgowdaa <sumanthsilence@gmail.com>
2024-01-31 01:11:43 +0530

    password gen updated

--- template/confirm.js
+++ template/confirm.js
@@ -1,23 +1,23 @@
 
-const reset_password =  (name, email, token) => {
+const confirm =  (name, email,sub, msg) => {
     return `<div>
                 <main>
                     <div>
                         <h1>Hello, ${name}, your email id is ${email} </h1>
-                        <h3>Follow this token to reset your password.. </h3>
+                        <h3> ${sub} </h3>
                         <p>
                             <strong>
-                                ${token}
+                                ${msg}
                             </strong>
                         </p>
 
                         <p>If you didn't ask to reset your password, ignore this link.</p>
 
                         <h3>Thanks,</h3>
-                        <h4>Sumanth Technologies.</h4>
+                        <h4>Team Sumanth Technologies.</h4>
                     </div>
                 </main>
             </div>`
 }
 
-module.exports = reset_password
+module.exports = confirm_temp
